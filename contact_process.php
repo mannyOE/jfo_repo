faa54f202b4df50479acddc22da9283cfb857e7d
ahmanemmanuel@gmail.com <ahmanemmanuel@gmail.com>
2018-07-20 14:14:49 +0100

    dfd

--- contact_process.php
+++ contact_process.php
@@ -3,7 +3,7 @@
     $to = "hpjackson@mailinator.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
+    $csubject = $_REQUEST['subject'];
     $number = $_REQUEST['number'];
     $cmessage = $_REQUEST['message'];
 
@@ -13,7 +13,7 @@
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your Sierra.";
+    $subject = "You have a message from your campain site.";
 
     $logo = '#';
     $link = '#';
